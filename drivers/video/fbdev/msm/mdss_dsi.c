028305b1d73967125cf2570fd7b735b2ea10f8e0
Rizky Benggolo <travarilo@gmail.com>
2019-01-04 22:00:37 +0700

    Revert "video: mdss: LiveDisplay driver"
    This reverts commit 895b797947c4d6ef20d64221f08bc10149ef5671.
    
    Change-Id: Ibf36a72b4e27dae5579362fe2d6165928889aafc

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -37,7 +37,6 @@
 #ifdef CONFIG_MACH_ASUS_X00T
 #include "mdss_panel.h"
 #endif
-#include "mdss_livedisplay.h"
 
 #define CMDLINE_DSI_CTL_NUM_STRING_LEN 2
 
@@ -3144,9 +3143,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		break;
 	case MDSS_EVENT_AVR_MODE:
 		mdss_dsi_avr_config(ctrl_pdata, (int)(unsigned long) arg);
-		break;
-	case MDSS_EVENT_UPDATE_LIVEDISPLAY:
-		rc = mdss_livedisplay_update(ctrl_pdata, (int)(unsigned long) arg);
 		break;
 	default:
 		pr_debug("%s: unhandled event=%d\n", __func__, event);
